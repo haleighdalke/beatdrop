0664da5dd44dd896120dde0c7ba60a8d589146c8
haleighdalke <haleighdalke@gmail.com>
2020-07-08 17:08:13 -0400

    step 2 of spotify api attempt

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,35 +1,62 @@
-class AuthController < ApplicationController
+class Api::V1::AuthController < ApplicationController
 
-
-    def login
-        if session[:user_id]
-            @user = User.find(session[:user_id])
-        else
-            @user = nil
-        end
+    def spotify_request
+        url = "https://accounts.spotify.com/authorize"
+        query_params = {
+            # double check encryption here
+            client_id: Rails.application.credentials[Rails.env.to_sym][:spotify][:client_id],
+            response_type: 'code',
+            redirect_uri: 'http://localhost:3000/api/v1/user',
+            scope: "user-library-read 
+            playlist-read-collaborative 
+            user-read-private 
+            user-top-read",
+            show_dialog: true
+        }
+        redirect_to "#{url}?#{query_params.to_query}"
     end
 
-    def verify_username
-        # find username 
-        # if exists, redirect to user show page
-        # else sign up (user new)
 
-        @user = User.find_by(name: params[:auth][:name])
 
-        if @user
-            session[:user_id] = @user.id
-            redirect_to @user
-        else
-            flash[:message] = "Please enter the proper credentials"
-            render :login
-            #redirect_to new_user_path
-        end
 
-    end
 
-    def logout
-        session.clear
-        redirect_to login_path
-    end
 
-end
+
+
+
+
+# class AuthController < ApplicationController
+
+
+#     def login
+#         if session[:user_id]
+#             @user = User.find(session[:user_id])
+#         else
+#             @user = nil
+#         end
+#     end
+
+#     def verify_username
+#         # find username 
+#         # if exists, redirect to user show page
+#         # else sign up (user new)
+
+#         @user = User.find_by(name: params[:auth][:name])
+
+#         if @user
+#             session[:user_id] = @user.id
+#             redirect_to @user
+#         else
+#             flash[:message] = "Please enter the proper credentials"
+#             render :login
+#             #redirect_to new_user_path
+#         end
+
+#     end
+
+#     def logout
+#         session.clear
+#         redirect_to login_path
+#     end
+
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
+
+  # OAuth
+  namespace :api do
+    namespace :v1 do
+      get '/login', to: "auth#spotify_request"
+      get '/auth', to: "auth#show"
+      get '/user', to: "users#create"
+      patch '/user', to: "users#update"
+    end
+  end
+
   resources :users
   resources :plays
   resources :genres
   resources :songs
   resources :artists
 
-  get '/login', to: 'auth#login', as: 'login'
-  post '/login', to: 'auth#verify_username'
+  # get '/login', to: 'auth#login', as: 'login'
+  # post '/login', to: 'auth#verify_username'
 
-  get '/logout', to: 'auth#logout', as: 'logout'
+  # get '/logout', to: 'auth#logout', as: 'logout'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
