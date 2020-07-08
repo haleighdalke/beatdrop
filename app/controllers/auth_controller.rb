ab9cfd2bf8d1ed7700c2928ccbf3a8493f3292ed
haleighdalke <haleighdalke@gmail.com>
2020-07-08 17:17:36 -0400

    half of create in auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -16,6 +16,30 @@ class Api::V1::AuthController < ApplicationController
         redirect_to "#{url}?#{query_params.to_query}"
     end
 
+    def create
+        # Request refresh and access tokens
+        body = {
+            grant_type: "authorization_code",
+            code: params[:code],
+            redirect_uri: 'http://localhost:3000/api/v1/user',
+            client_id: Rails.application.credentials[Rails.env.to_sym][:spotify][:client_id],
+            client_secret: Rails.application.credentials[Rails.env.to_sym][:spotify][:client_secret]
+        }
+
+        auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
+        auth_params = JSON.parse(auth_response.body)
+        header = {
+            Authorization: "Bearer #{auth_params["access_token"]}"
+        }
+
+        user_response = RestClient.get("https://api.spotify.com/v1/me", header)
+        user_params = JSON.parse(user_response.body)
+
+        #---
+        # Create User
+        
+    end
+end
 
 
 
