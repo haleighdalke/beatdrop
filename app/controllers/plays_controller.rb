7cc2b487b8099c50ef38449424f51bc3c350fda0
haleighdalke <haleighdalke@gmail.com>
2020-07-07 20:32:52 -0400

    Plays button NOT FINISHED

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,16 @@
 class PlaysController < ApplicationController
+
+    def create
+
+        if session[:user_id] != nil
+            @play = Play.new(user_id: session[:user_id], song_id: params[:song_id])
+            @play.num_plays != nil ? @play.num_plays += 1 : @play.num_plays = 1
+            @play.save
+            redirect_to user_path(User.find(session[:user_id]))
+        else
+            flash[:message] = "You must login to play a song"
+            redirect_to login_path
+        end
+
+    end
 end
