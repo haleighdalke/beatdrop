c2d264c13653ced048d6ef75aa623fdfdcc5e764
haleighdalke <haleighdalke@gmail.com>
2020-07-09 13:24:09 -0400

    DRY code: user auth helper in applicationcontroller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,11 +2,6 @@ class AuthController < ApplicationController
 
 
     def login
-        if session[:user_id]
-            @user = User.find(session[:user_id])
-        else
-            @user = nil
-        end
     end
 
     def verify_username
@@ -20,9 +15,8 @@ class AuthController < ApplicationController
             session[:user_id] = @user.id
             redirect_to @user
         else
-            flash[:message] = "Please enter the proper credentials"
+            flash[:message] = "Invalid username. Please enter the proper credentials"
             render :login
-            #redirect_to new_user_path
         end
 
     end
